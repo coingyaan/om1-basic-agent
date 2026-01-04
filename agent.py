9c2e58048f3dbdca6f4f98c73e287b9a45627c48
CoinGyaan <62913922+coingyaan@users.noreply.github.com>
2026-01-05 00:22:38 +0530

    Add basic OM1  agent

---
+++ agent.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+def run_agent():
+    print("Hello from OM1 Basic Agent")
+    print("Agent status: running")
+    print("Current system time:", datetime.now())
+
+if __name__ == "__main__":
+    run_agent()
